7cb1da1f7a6eea2011c60e721c8d59b40c604f0b
Fabrício Júnio <junioad555@gmail.com>
2026-05-24 12:47:27 -0300

    feat(security): sem source maps, sem console e robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    drop: ["console", "debugger"],
+  },
+  build: {
+    sourcemap: false,
+  },
 });
