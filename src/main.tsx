eccc845a8372d146626f82258b14b3d3455e6fc4
Fabrício Júnio <junioad555@gmail.com>
2026-05-30 17:00:04 -0300

    feat: adiciona portfólio oficial em / e mantém IDE em /lab

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/jetbrains-mono/400.css";
 import "@fontsource/jetbrains-mono/500.css";
-import "@fontsource/jetbrains-mono/600.css";
+import "@fontsource/instrument-serif/400.css";
+import "@fontsource/instrument-serif/400-italic.css";
 import "./index.css";
 import App from "./App";
 
